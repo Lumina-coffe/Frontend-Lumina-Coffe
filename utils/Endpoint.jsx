329b094533b78d62267f0334258cdb919772397c
Viriniawan Lionel M <virgimessi11@gmaill.com>
2025-04-15 10:47:34 +0700

    fix: endpoint

--- utils/Endpoint.jsx
+++ utils/Endpoint.jsx
@@ -1,5 +1,5 @@
 export const BASEURL = 'https://backend-lumina-coffe.vercel.app/';
 
 export const URL_SIGNIN = `${BASEURL}api/auth/signin`;
-export const URL_PRODUCT = `${BASEURL}/api/products`;
-export const URL_TRANSACTION = `${BASEURL}/api/transactions`;
+export const URL_PRODUCT = `${BASEURL}api/products`;
+export const URL_TRANSACTION = `${BASEURL}api/transactions`;
